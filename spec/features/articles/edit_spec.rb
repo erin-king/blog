6f97ae3ce38ad4b00fc347bf960a650355f47d4b
Erin King <44042415+erin-king@users.noreply.github.com>
2019-06-26 13:50:42 -0600

    Add edit spec for article

---
+++ spec/features/articles/edit_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'user can edit an article' do
+  describe 'user visits article show page' do
+    describe 'user clicks on Edit Article link' do
+      it 'can change the title and body of an article' do
+        article_1 = Article.create!(title: "Title 1", body: "Body 1")
+        article_2 = Article.create!(title: "Title 2", body: "Body 2")
+
+        visit articles_path
+
+        click_link article_1.title
+
+        click_link 'Edit Article'
+
+        fill_in article[title], with: 'Edited Title'
+        fill_in article[body], with: 'Edited Body'
+        click_link 'Update Article'
+
+        expect(current_path).to eq(article_path(article_1))
+        expect(page).to have_content("Edited Title")
+        expect(page).to have_content("Edited Body")
+      end
+    end
+  end
+end
