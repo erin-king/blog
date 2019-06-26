35af5e948ff0fd0d961a5651a3849b0018b9bc3b
Erin King <44042415+erin-king@users.noreply.github.com>
2019-06-26 13:36:46 -0600

    Add delete to show spec, modified how page, add destroy action to articles controller

--- spec/features/articles/show_spec.rb
+++ spec/features/articles/show_spec.rb
@@ -2,18 +2,30 @@ require 'rails_helper'
 
 describe 'user sees one article' do
   describe 'user links from article index' do
-    it 'displays info for one article' do
-      article_1 = Article.create!(title: "Title 1", body: "Body 1")
-      article_2 = Article.create!(title: "Title 2", body: "Body 2")
+    before :each do
+      @article_1 = Article.create!(title: "Title 1", body: "Body 1")
+      @article_2 = Article.create!(title: "Title 2", body: "Body 2")
 
       visit articles_path
 
-      click_link article_1.title
+      click_link @article_1.title
+    end
 
-      expect(current_path).to eq(article_path(article_1))
-      expect(page).to have_content(article_1.title)
-      expect(page).to have_content(article_1.body)
-      expect(page).to_not have_content(article_2.title)
+    it 'displays info for one article' do
+      expect(current_path).to eq(article_path(@article_1))
+      expect(page).to have_content(@article_1.title)
+      expect(page).to have_content(@article_1.body)
+      expect(page).to_not have_content(@article_2.title)
+    end
+
+    it 'has a delete link that deletes the article' do
+      expect(page).to have_link("Delete Article")
+
+      click_link "Delete Article"
+
+      expect(current_path).to eq(articles_path)
+      expect(page).to have_content(@article_2.title)
+      expect(page).to_not have_content(@article_1.title)
     end
   end
 end
