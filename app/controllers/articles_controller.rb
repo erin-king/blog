b7a39eda9265d662c0e1250f407e96360ff345e1
Erin King <44042415+erin-king@users.noreply.github.com>
2019-06-26 13:10:50 -0600

    Add create action in articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,12 @@
 class ArticlesController < ApplicationController
   def new
+    @article = Article.new
+  end
 
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
   end
 
   def index
@@ -9,5 +15,11 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
