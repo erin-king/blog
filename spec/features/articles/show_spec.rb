2d187b2d51716ad22cc43b20649bf6eb7c489d37
Erin King <44042415+erin-king@users.noreply.github.com>
2019-07-01 11:17:10 -0600

    Add comment partial for articles

--- spec/features/articles/show_spec.rb
+++ spec/features/articles/show_spec.rb
@@ -14,7 +14,6 @@ describe 'user sees one article' do
     end
 
     it 'displays info for one article' do
-      save_and_open_page
       expect(current_path).to eq(article_path(@article_1))
       expect(page).to have_content(@article_1.title)
       expect(page).to have_content(@article_1.body)
