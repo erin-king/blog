0229d2b8d209f11b6398cdea14e49937d333c29e
Erin King <44042415+erin-king@users.noreply.github.com>
2019-08-12 10:30:33 -0600

    Add tag and tagging models as well as relationships

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ApplicationRecord
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
   validates_presence_of :title
   validates_presence_of :body
 end
